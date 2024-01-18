8fa61aadee3ec44a497032d6b3acb29f84cb0dab
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2024-01-19 01:38:41 +0900

    ADD : 블록체인의 블록을 만들기위한 기초 코드 작성.

--- build/index.js
+++ build/index.js
@@ -1,9 +1,2 @@
-const hello = () => 'hi';
-class Block {
-    constructor(data) {
-        this.data = data;
-    }
-    static hello() {
-        return "안녕!";
-    }
-}
+"use strict";
+console.log("bye");
